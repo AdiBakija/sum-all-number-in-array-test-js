b2b8bfe59856b1db29423b09cefea54d5ef4954c
Adi <adibakija35@hotmail.com>
2018-10-24 21:28:30 +0000

    Completed array recursion exercise

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  var sum = 0;
+  array.forEach((item) => {
+
+    if (Array.isArray(item)) {
+      // Print out all it's items individually
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+
+  });
+  return sum;
 }
 
 module.exports = sumItems;
